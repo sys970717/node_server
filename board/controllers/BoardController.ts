6d6c5bbf6f211ef6cda575ffe2f5d6f2c3ecf62c
Seoyeongsu <sys970717@naver.com>
2020-01-16 19:00:44 +0900

    게시판 라우터, 컨트롤러 작성중  - 라우터, 컨트롤러 작성중.

--- board/controllers/BoardController.ts
+++ board/controllers/BoardController.ts
@@ -1,8 +1,22 @@
+import { Request, Response } from 'express'
+
 class BoardController {
     public ddd: string
     
     constructor() {
+        
+    }
 
+    public write = async(req: Request, res: Response) => {
+        console.log('write');
+    }
+
+    public view = async(req: Request, res: Response) => {
+        // OAuth2.0
+        if(req.headers.authorization)
+        res.json({
+            'id': req.param('id')
+        });
     }
 }
 
--- board/routes/BoardRouter.ts
+++ board/routes/BoardRouter.ts
@@ -6,7 +6,14 @@ class BoardRouter {
     public router: express.Router
 
     constructor() {
+        const bodyParserJson = bodyParser.json()
+        console.log('Init > Board Router Loading')
         this.router = express.Router()
+
+        let boardController = new BoardController()
+
+        this.router.post('/write', bodyParserJson, boardController.write)
+        this.router.get('/view', bodyParserJson, boardController.view)
     }
 }
 
